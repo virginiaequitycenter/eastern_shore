ea19f9239dbc1c1850925a74fa9ac525e15fe604
Michele Claibourn <mclaibourn@virginia.edu>
2022-07-04 14:23:27 -0400

    add atlas/shiny

---
+++ easternshore-dashboard/eastern-shore/deploy.R
@@ -0,0 +1,9 @@
+rsconnect::deployApp(appName = "eastern-shore-atlas", 
+                     appFiles = c("app.R", 
+                                  "www/app_data.Rdata", "www/boostrap.css"),
+                     account = "virginiaequitycenter") 
+
+# rsconnect::deployApp(appName = "eastern-shore-atlas", 
+#                      appFiles = c("app.R", 
+#                                   "www/app_data.Rdata", "www/boostrap.css"),
+#                      account = "commpaslab") 
