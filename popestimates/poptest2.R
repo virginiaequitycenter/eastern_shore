61f0360415ace44544b7f43ff82116eec1db65c2
mclaibourn <mclaibourn@virginia.edu>
2025-06-10 15:19:35 -0400

    update pop impact example

--- popestimates/poptest2.R
+++ popestimates/poptest2.R
@@ -14,19 +14,13 @@ pal <- c("#FEF0D9", "#FDD49E", "#FDBB84", "#FC8D59", "#E34A33", "#B30000")
 
 
 # Read data ----
+box_auth()
+
 ## Population data ----
 # does this need to be formalized in box?
 # currently read locally, added to repo
-pop <- read_csv("block_pop_data.csv") %>% 
-  mutate(GEOID = as.character(GEOID))
-
-## Block shapefile ----
-# does this need to be formalized in box?
-# currently read locally, 
-# but available in box as geojson
-# https://virginia.app.box.com/file/1669511851078 
-# (though not readable by box_read function)
-blocks <- st_read("cb/esva_2020block_clipped.geojson")
+pop <- box_read_csv("1883093293417") %>% 
+  mutate(GEOID20 = as.character(GEOID))
 
 ## Groundwater data ----
 # Choose one of Farshad's files
@@ -34,15 +28,13 @@ blocks <- st_read("cb/esva_2020block_clipped.geojson")
 gw_json_export <- box_read_json("1869334139989") # read in single ea_export json file
 gw_json_boxid <- str_extract(gw_json_export$data$path, "(\\d+)") # get id of csv json file
 gw_json_csv <- box_read_json(gw_json_boxid) # read in csv json file
-gw_csv_boxid <- str_extract(gw_json_csv$path, "(\\d+)") # get id of csv data file
+gw_csv_id <- str_extract(gw_json_csv$path, "(\\d+)") # get id of csv data file
 gw_csv <- box_read_csv(gw_csv_id) %>% # read data file
   mutate(GEOID20 = as.character(GEOID20))
 
 ## Combine data ----
 # sum(gw_csv$GEOID20 %in% blocks$GEOID20) # 2647
-gw_df <- left_join(blocks, pop, by=c("GEOID20"="GEOID")) %>%
-  left_join(gw_csv, by = c("GEOID20"="GEOID20")) %>% 
-  st_drop_geometry()
+gw_df <- left_join(gw_csv, pop, by=c("GEOID20"="GEOID20")) 
 
 
 # Generate impacted pop estimates ----
@@ -73,7 +65,7 @@ bin_function <- function(var_name) {
 }
 
 # create bin_ variable for each variable identified in ea json
-gw_df_test <- gw_df %>% 
+gw_df <- gw_df %>% 
   mutate(across(all_of(var_name), bin_function,
                 .names = "bin_{.col}"),
          event = gw_json_export$event)
@@ -82,9 +74,12 @@ gw_df_test <- gw_df %>%
 ### create summary df by variable bins ----
 # TODO, make this mappable across group_vars
 # e.g., use syms() so objects are understandable as data frame column names
-group_vars <- syms(gw_df_test %>% select(starts_with("bin")) %>% names())
+group_vars <- syms(gw_df %>% select(starts_with("bin")) %>% names())
+# group_vars2 <- as.vector(gw_df %>% select(starts_with("bin")) %>% names())
+# group_vars_description <- as.vector(gw_json_export$dataColumns$`$description`)
+
 # to create n_x summary data frames... but for now try one
-gw_df_var1 <- gw_df_test %>% 
+gw_df_test <- gw_df %>% 
   group_by(!!group_vars[[1]], event) %>% 
   summarize(across(c(total:jobs_wac_hi), sum)) %>% 
   mutate(across(c(hisp:pop_under18), ~ (.x/total)*100, .names = "per_{.col}"),
@@ -94,14 +89,14 @@ gw_df_var1 <- gw_df_test %>%
   ungroup() %>% 
   mutate(per_total = (total/sum(total))*100,
          per_housing = (total_housing/sum(total_housing))*100) %>% 
-  rename(bin = !!group_vars[[1]]) %>% 
+  rename(bin = group_vars[[1]]) %>% 
   bind_rows(pop_total) %>% 
   mutate(bin = factor(bin, levels = c(legend_labels, "ESVA Total")))
 
 
 # Example visual ----
 ## Single Bar ----
-gw_df_var1 %>% 
+gw_df_test %>% 
   filter(event != "none") %>% 
   ggplot(aes(x = bin, y = per_hisp)) +
   geom_col(aes(fill = bin)) +
@@ -109,12 +104,12 @@ gw_df_var1 %>%
             vjust = 1) +
   scale_fill_manual(values = pal, na.value="grey", guide = "none") +
   expand_limits(x= c(1, 8)) +
-  geom_hline(aes(yintercept = gw_df_var1_total %>% 
+  geom_hline(aes(yintercept = gw_df_test %>% 
                    filter(event == "none") %>% 
                    pull(per_hisp)), 
              linetype = 2, color = "brown4") +
   annotate("text", x = 7, label = "ESVA\nTotal", color = "brown4",
-           y = gw_df_var1_total %>% 
+           y = gw_df_test %>% 
              filter(event == "none") %>% 
              pull(per_hisp)) +   
   labs(title = "Percent of Residents: Hispanic",
@@ -126,12 +121,12 @@ gw_df_var1 %>%
 ## Bars over all popvars ----
 # probably need to further limit the pop variables
 # proposed for removal: 
-#   amind_aknat, asian, nathw_pacis, other_race,
+#   amind_aknat, asian?, nathw_pacis, other_race,
 #   pop_over18, jobs_rac_low, jobs_rac_mid, jobs_rac_hi
-#   possibly: jobs_wac_mid, jobs_wac_hi 
+#   possibly: jobs_wac_hi 
 
 ## all except population and housing totals
-popvars <- select(gw_df_var1_total, starts_with("per")) %>% names()
+popvars <- select(gw_df_test, starts_with("per")) %>% names()
 poplabels <- c("Hispanic", "White", "Black", "American Indian/Alaskan Native",
                "Asian", "Native Hawaiian/Pacific Islander", "Remaining Racial Categories",
                "Multiracial", "Over 18", "Under 18", "Occupied Housing",
@@ -141,19 +136,19 @@ poplabels <- c("Hispanic", "White", "Black", "American Indian/Alaskan Native",
                "Total Population", "Total Housing Stock")
 
 bar_plots <- map2(popvars[1:17], poplabels[1:17], 
-                        ~ gw_df_var1 %>% 
+                        ~ gw_df_test %>% 
                           filter(event != "none") %>% 
                           ggplot(aes(x = bin, y = .data[[.x]])) +
                           geom_col(aes(fill = bin)) +
                           geom_text(aes(y = .data[[.x]], 
                                         label = round(.data[[.x]], 1)),
                                     vjust = 1, color = "black") +
-                          geom_hline(aes(yintercept = gw_df_var1 %>% 
+                          geom_hline(aes(yintercept = gw_df_test %>% 
                                            filter(bin == "ESVA Total") %>% 
                                            pull(.x)), color = "brown4", linetype = 2) +
                           expand_limits(x= c(1, 8)) +
                           annotate("text", x = 7, label = "ESVA\nTotal", color = "brown4",
-                                   y = gw_df_var1 %>% 
+                                   y = gw_df_test %>% 
                                      filter(event == "none") %>% 
                                      pull(.x)) +   
                           scale_fill_manual(values = pal, na.value="grey", guide = "none") +
@@ -167,7 +162,7 @@ bar_plots[17]
 
 # prop_total and prop_housing shouldn't have total reference line
 bar_plots_tot <- map2(popvars[18:19], poplabels[18:19], 
-                            ~ gw_df_var1 %>% 
+                            ~ gw_df_test %>% 
                               filter(event != "none") %>% 
                               ggplot(aes(x = bin, y = .data[[.x]])) +
                               geom_col(aes(fill = bin)) +
@@ -181,3 +176,21 @@ bar_plots_tot <- map2(popvars[18:19], poplabels[18:19],
                               theme_classic())
 bar_plots_tot[1]
 # wrap_plots(bar_plots_tot)
+
+# Additional elements for RMD file ----
+region_bbox <- gw_json_export$regionBoundingBox
+bbox <- c(region_bbox$lonMin, region_bbox$latMin, region_bbox$lonMax, region_bbox$latMax)
+
+descriptionTitle <- gw_json_export$descriptionTitle
+
+var1_name <- gw_json_export$dataColumns$name[1]
+var1_name_index <- match(var1_name, gw_json_csv$schema$fields$name)
+var1_title <- gw_json_csv$schema$fields$title[var1_name_index]
+
+# remove objects not needed for RMD file ----
+rm(bar_plots, bar_plots_tot, group_vars, gw_csv, pop, pop_total,
+   gw_csv_id, gw_json_boxid, pal, var_name, bin_function,
+   gw_json_csv, gw_json_export)
+
+# Save for use in rmd file ----
+save.image("gw_pop_example.Rdata")
