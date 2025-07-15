a83e95fece9cfc4113ba9c5ee83826ef2284fd48
eam5 <eam5hc@virginia.edu>
2025-07-15 10:43:31 -0400

    add livability tools prototype

---
+++ easternshore-dashboard/esva-livability-tool/app-data-prep.R
@@ -0,0 +1,209 @@
+# Libraries
+library(here)
+library(shiny)
+library(tidyverse)
+library(boxr)
+library(jsonlite) 
+library(sf)
+library(leaflet)
+library(RColorBrewer)
+library(bslib)
+library(qs)
+
+# Set WD
+setwd(here("esva-livability-tool"))
+
+# Box authentication
+readRenviron("~/.Renviron")
+box_auth(client_id = Sys.getenv('BOX_CLIENT_ID'), client_secret = Sys.getenv('BOX_CLIENT_SECRET'))
+
+# Get Box Data
+dir_id <- "305971996036" # EA_Export
+box_setwd(dir_id)
+
+# box_ls()
+
+box_fetch(
+  dir_id = box_getwd(),
+  local_dir = "app_data/app_data_updated",
+  recursive = TRUE,
+  overwrite = TRUE,
+  delete = TRUE
+)
+
+# Data prep ----
+
+# Get Box ID function
+get_boxid <- function(path) {
+  boxid <- str_split(path, "https://virginia.app.box.com/file/", simplify = TRUE)[2]
+  boxid
+}
+
+
+block_geo <- st_read("data/blocks/esva_2020block_clipped.geojson")
+block_geo <- st_transform(block_geo, 4326) %>% 
+  select(GEOID20)
+
+# Data prep function
+ea_prep_func <- function(eafile) {
+  
+  # Read in ea_export
+  ea_export <- fromJSON(paste0("app_data/app_data_updated/", eafile))
+  
+  # Get CSV-JSON
+  json_path <- ea_export$data$path
+  json_id <- get_boxid(json_path)
+  json <- box_read_json(file_id = json_id)
+  
+  # Get CSV
+  csv_id <- get_boxid(json$path)
+  csv <- box_read_csv(csv_id)
+  
+  # Join geometry
+  csv_geo <- csv %>% 
+    mutate(GEOID20 = as.character(GEOID20)) %>% 
+    left_join(block_geo, by = join_by(GEOID20 == GEOID20)) %>% 
+    st_as_sf()
+  
+  # Gather data for app
+  region_bbox <- ea_export$regionBoundingBox
+  bbox <- c(region_bbox$lonMin, region_bbox$latMin, region_bbox$lonMax, region_bbox$latMax)
+  
+  year <- ea_export$data$key
+  descriptionTitle <- ea_export$descriptionTitle
+  event <- ea_export$event
+  description <- ea_export$descriptionText
+  
+ measures <- list() 
+  
+  for (i in seq_along(ea_export$dataColumns$name)) {
+    name <- ea_export$dataColumns$name[i]
+    name_index <- match(name, json$schema$fields$name)
+    title <- json$schema$fields$title[name_index]
+    field_description <- json$schema$fields$description[name_index]
+    # print(title)
+    
+    var <- quo(name)
+    map_data <- csv_geo %>% select(GEOID20, UQ(var), geometry)
+    
+    # print(dat)
+
+    legend_breaks <- as.vector(ea_export$dataColumns$bins[[i]])
+    legend_labels <- as.list(ea_export$dataColumns$labels[[i]])
+
+    ls_name <- as.character(title)
+    ls_name
+    ls <- list(name=name, map_data=map_data, title=title, field_description=field_description, 
+               legend_breaks=legend_breaks, legend_labels=legend_labels)
+    # print(ls)
+    # measures <- list(title=ls)
+    measures <- append(measures, setNames(list(ls), as.character(title)))
+    # measures <- append(measures, list(title=ls))
+# print(measures)
+  }
+  
+# print(measures)
+  
+  file_prep <- list(year=year, event=event, description=description, 
+                    descriptionTitle=descriptionTitle, bbox=bbox,
+                    measures=measures)
+  
+  file_prep
+  
+}
+
+# Groundwater
+groundwater_2024 <- ea_prep_func(list.files("app_data/app_data_updated")[1])
+groundwater_2030 <- ea_prep_func(list.files("app_data/app_data_updated")[2])
+groundwater_2040 <- ea_prep_func(list.files("app_data/app_data_updated")[3])
+groundwater_2050 <- ea_prep_func(list.files("app_data/app_data_updated")[4])
+groundwater_2060 <- ea_prep_func(list.files("app_data/app_data_updated")[5])
+groundwater_2080 <- ea_prep_func(list.files("app_data/app_data_updated")[6])
+
+groundwater <- list("2024"=groundwater_2024, "2030"=groundwater_2030, 
+                    "2040"=groundwater_2040, "2050"=groundwater_2050, 
+                    "2060"=groundwater_2060, "2080"=groundwater_2080)
+
+# Storm Surge: Hurricane Dorian
+dr_2019 <- ea_prep_func(list.files("app_data/app_data_updated")[7])
+dorian <- list("2019"=dr_2019)
+
+# Storm Surge Isabel
+ib_2003 <- ea_prep_func(list.files("app_data/app_data_updated")[8])
+ib_2025 <- ea_prep_func(list.files("app_data/app_data_updated")[10])
+ib_2030 <- ea_prep_func(list.files("app_data/app_data_updated")[11])
+ib_2040 <- ea_prep_func(list.files("app_data/app_data_updated")[12])
+ib_2050 <- ea_prep_func(list.files("app_data/app_data_updated")[13])
+ib_2060 <- ea_prep_func(list.files("app_data/app_data_updated")[14])
+ib_2080 <- ea_prep_func(list.files("app_data/app_data_updated")[15])
+
+isabel <- list("2003"=ib_2003, "2025" = ib_2025, "2030"=ib_2030, 
+               "2040"=ib_2040, "2050"=ib_2050, 
+               "2060"=ib_2060, "2080"=ib_2080)
+
+# Storm Surge Hurricane Joaquin
+jq_2015 <- ea_prep_func(list.files("app_data/app_data_updated")[16])
+joaquin <- list("2015"=jq_2015)
+
+# Storm Surge King Tide
+kt_2009 <- ea_prep_func(list.files("app_data/app_data_updated")[17])
+kingtide <- list("2009"=kt_2009)
+
+# Storm Surge Nor'Ida Storm
+ni_2009 <- ea_prep_func(list.files("app_data/app_data_updated")[18])
+norida <- list("2009"=ni_2009)
+
+# Extreme Wetness/Dryness
+ewd_2025 <- ea_prep_func(list.files("app_data/app_data_updated")[19])
+ewd_2030 <- ea_prep_func(list.files("app_data/app_data_updated")[20])
+ewd_2040 <- ea_prep_func(list.files("app_data/app_data_updated")[21])
+ewd_2050 <- ea_prep_func(list.files("app_data/app_data_updated")[22])
+ewd_2060 <- ea_prep_func(list.files("app_data/app_data_updated")[23])
+ewd_2080 <- ea_prep_func(list.files("app_data/app_data_updated")[24])
+
+extremes <- list("2025" = ewd_2025, "2030"=ewd_2030, 
+                 "2040"=ewd_2040, "2050"=ewd_2050, 
+                 "2060"=ewd_2060, "2080"=ewd_2080)
+
+# Roadway flooding
+rdflood_2040 <- ea_prep_func(list.files("app_data/app_data_updated")[32])
+rdflood_2060 <- ea_prep_func(list.files("app_data/app_data_updated")[33])
+rdflood_2080 <- ea_prep_func(list.files("app_data/app_data_updated")[34])
+
+roadflood <- list("2040"=rdflood_2040, "2060"=rdflood_2060, "2080"=rdflood_2080)
+
+# Water Level Depth
+wld_2020 <- ea_prep_func(list.files("app_data/app_data_updated")[35])
+avg_wld <- list("2020-2023"=wld_2020)
+# Septic System Risk Assessment
+ssra_2020 <- ea_prep_func(list.files("app_data/app_data_updated")[36])
+septic <- list("2020-2023"=ssra_2020)
+
+# Seawater intrusion
+swi_2024 <- ea_prep_func(list.files("app_data/app_data_updated")[37])
+swi_2030 <- ea_prep_func(list.files("app_data/app_data_updated")[38])
+swi_2040 <- ea_prep_func(list.files("app_data/app_data_updated")[39])
+swi_2050 <- ea_prep_func(list.files("app_data/app_data_updated")[40])
+swi_2060 <- ea_prep_func(list.files("app_data/app_data_updated")[41])
+swi_2080 <- ea_prep_func(list.files("app_data/app_data_updated")[42])
+
+swi <- list("2024" = swi_2024, "2030"=swi_2030, 
+                    "2040"=swi_2040, "2050"=swi_2050, 
+                    "2060"=swi_2060, "2080"=swi_2080)
+
+
+# Compile App data ----
+# app_dat <- list(groundwater_2030=groundwater_2030, groundwater_2040=groundwater_2040)
+app_dat <- list(`Depth to Groundwater`=groundwater, `Extreme Wetness/Dryness`=extremes, 
+                `Roadway Flooding`=roadflood,
+                `Seawater Intrusion`=swi, 
+                `Storm Surge: Hurricane Dorian`=dorian, `Storm Surge: Hurricane Isabel`=isabel,
+                `Storm Surge: Hurricane Joaquin`=joaquin, `Storm Surge: King Tide`=kingtide,
+                `Storm Surge: Nor'Ida Storm`=norida, 
+                `Case Study Areas: Average Water Level Depth`=avg_wld,
+                `Case Study Areas: Septic System Risk Assessment`=septic
+                )
+
+  
+  
+qs::qsave(app_dat, "app_data_test.qs")
---
+++ easternshore-dashboard/esva-livability-tool/app.R
@@ -0,0 +1,262 @@
+# Eastern Shore of Virginia Livability Tool
+
+# Libraries
+library(here)
+library(shiny)
+library(tidyverse)
+library(boxr)
+library(jsonlite) 
+library(sf)
+library(leaflet)
+library(leaflet.extras2)
+library(RColorBrewer)
+library(bslib)
+
+# Set WD
+setwd(here("esva-livability-tool"))
+
+# Read in data
+app_dat <- qs::qread("app_data_test.qs")
+
+input_choices = names(app_dat) 
+input_years = names(app_dat[[1]])
+input_measures = names(app_dat[[1]][[1]][["measures"]])
+
+ui <- page_sidebar(
+  title = "ESVA Livability Tool",
+  fillable = FALSE,
+  sidebar = sidebar(
+    selectInput(
+            'scenario_ww',
+            label = 'Topic',
+            choices = input_choices,
+            selected = input_choices[1]
+          ),
+    selectInput(
+      'scenario_yr',
+      label = 'Year',
+      choices = input_years
+      ),
+    selectInput(
+      'scenario_m',
+      label = 'Measure',
+      choices = input_measures
+      )
+    ),
+  layout_columns(
+    # col_widths = 12,
+    col_widths = c(8,4),
+    row_heights = c(1),
+    leafletOutput('map', width="100%", height = "600px"),
+    htmlOutput("scenario_meta")
+    
+    )
+  
+) # end page_navbar
+
+server <- function(input, output, session){
+  
+  scenario_ww <- reactive({
+    for (i in seq_along(input_choices)) {
+      if (input$scenario_ww == names(app_dat)[i]) {
+        d <- app_dat[[i]]
+      }
+    }
+    d
+  })
+  
+  observeEvent(scenario_ww(), {
+    choices <- names(scenario_ww())
+    freezeReactiveValue(input, "scenario_yr")
+    updateSelectInput(inputId = "scenario_yr", choices = choices)
+  })
+  
+  scenario_yr <- reactive({
+    req(input$scenario_yr)
+    for (i in seq_along(scenario_ww())) {
+      if (input$scenario_yr == names(scenario_ww())[i]) {
+        d <- scenario_ww()[[i]][["measures"]]
+      }
+    }
+    d
+    # print(d)
+  })
+  
+  observeEvent(scenario_yr(), {
+    choices <- names(scenario_yr())
+    freezeReactiveValue(input, "scenario_m")
+    updateSelectInput(inputId = "scenario_m", choices = choices)
+  })
+  
+ dw <- reactive({
+    yr <- as.character(input$scenario_yr)
+    d <- scenario_ww()[[`yr`]]
+    d
+  })
+ 
+ dm <- reactive({
+   ms <- as.character(input$scenario_m)
+   d <- dw()[["measures"]][[`ms`]]
+   d
+   # print(d)
+ })
+
+ event_name <- reactive({
+   d <- dw()
+   d <- d$event
+ })
+
+ event_title <- reactive({
+   d <- dw()
+   d <- d$descriptionTitle
+ })
+ 
+ event_meas_descript <- reactive({
+   d <- dm()
+   d <- d$field_description
+   print(d)
+ })
+ 
+ event_year <- reactive({
+   d <- dw()
+   d <- d$year
+ })
+
+ event_description <- reactive({
+   d <- dw()
+   d <- d$description
+ })
+
+ output$scenario_meta <- renderUI({
+   HTML(paste0('<span><small>Selected Measure:</small><br/><b>', event_title(), '</b></span><br/><br/>',
+               '<span><small>Selected Measure Description:</small><br/>', event_meas_descript(), '</span><br/><br/>',
+               '<span><small>Selected Year:</small><br/>', event_year(), '</span><br/><br/>',
+               '<span><small>More Information:</small><br/>', event_description(), '</span><br/>'
+   )
+   )
+ })
+  
+
+  # Map Functions -------------------------------------------------------
+  ## Leaflet base map function ----
+  
+ esva_bbox <- c(-76.06697 , 37.04853, -75.16060,  38.04154)
+
+  renderLeafletFunction <- function(map) {
+    renderLeaflet({
+      leaflet() %>%
+        addProviderTiles('CartoDB.Positron') %>%
+        fitBounds(esva_bbox[1], esva_bbox[2], esva_bbox[3], esva_bbox[4]) %>%
+        addResetMapButton()
+    })
+  }
+
+  ## leafletProxy Map Function ----
+  mapProxyFunction <- function(mapData, mapId,
+                               var, var_title,
+                               # descriptionTitle, 
+                               legend_labels,
+                               # bg_list, 
+                               pal, sel_range){
+
+    # map proxy
+    proxy <- leafletProxy(mapId, data = mapData)
+
+    # observe
+    observe({
+      proxy %>%
+        # fitBounds(bbox[1], bbox[2], bbox[3], bbox[4]) %>%
+        clearShapes() %>%
+        addPolygons(data = mapData,
+                    weight = 0.5,
+                    color = "#FFFFFF",
+                    smoothFactor = 0.2,
+                    fillOpacity = 0.6,
+                    fillColor = ~pal(var),
+                    label = lapply(as.list(var), HTML),
+                    group = var_title) %>%
+        clearControls() %>%
+        addLegend(position = 'topright',
+                  pal = pal,
+                  values = sel_range,
+                  labFormat = function(type, breaks) {
+                    return(legend_labels)
+                  },
+                  title = ~gsub("\n", "<br>",
+                                stringr::str_wrap(var_title,
+                                                  width = 20,
+                                                  whitespace_only = FALSE)),
+                  opacity = 0.6)
+        # addLayersControl(
+        #   baseGroups = bg_list,
+        #   options = layersControlOptions(collapsed = FALSE)
+        # )
+    })
+  }
+
+  listen_scen <- reactive(input$scenario_ww)
+
+  listen_yr <- reactive(input$scenario_yr)
+  
+  listen_m <- reactive(input$scenario_m)
+
+
+  # Build Map -------------------------------------------------------
+
+  # render leaflet map
+  output$map <- renderLeafletFunction()
+
+  observeEvent(list(listen_scen(), listen_yr(), listen_m()), {
+
+    p <- dm()
+    
+    # print(p)
+
+    name <- as.character(p$name)
+    var <- p$map_data[[`name`]]
+
+    breaks <- p$legend_breaks
+
+    sel_range <- c(min(breaks), max(breaks))
+
+    pal <- colorBin(c("#FEF0D9", "#FDD49E", "#FDBB84", "#FC8D59", "#E34A33", "#B30000"),
+                    sel_range,
+                    bins = breaks,
+                    right = TRUE,
+                    # reverse = TRUE,
+                    na.color = "#808080",
+                    pretty = FALSE )
+    
+    mapProxyFunction(p$map_data, "map", var, p$title, p$legend_labels, pal, sel_range)
+      
+    })
+
+  
+  # Map Reset button function ----
+  addResetMapButton <- function(leaf) {
+    leaf %>%
+      addEasyButton(
+        easyButton(
+          icon = "ion-arrow-expand",
+          title = "Reset View",
+          onClick = JS(
+            "function(btn, map){ map.setView(map._initialCenter, map._initialZoom); }"
+          )
+        )
+      ) %>%
+      htmlwidgets::onRender(
+        JS(
+          "function(el, x){
+            var map = this;
+            map.whenReady(function(){
+              map._initialCenter = map.getCenter();
+              map._initialZoom = map.getZoom();
+            });
+          }"
+        )
+      )
+  }
+  
+}
+
+shinyApp(ui, server)
